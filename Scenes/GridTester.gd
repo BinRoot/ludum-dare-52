0e9dd792c73e20bbda255a9724208d65a4f89280
Nishant Shukla <nick722@gmail.com>
2023-01-09 06:54:03 -0800

    money font

--- Scenes/GridTester.gd
+++ Scenes/GridTester.gd
@@ -11,6 +11,7 @@ onready var day_count_timer : Timer = $DayCountDown
 onready var day_end_audio : AudioStreamPlayer = $DayEndAudio
 onready var lights_out_tween : Tween = $LightsOutTween
 onready var lights_out_audio : AudioStreamPlayer = $LightsOutAudio
+onready var money_tween : Tween = $MoneyTween
 var num_times_horn_played = 0
 var num_bells = 2
 var is_already_quitting = false
@@ -44,7 +45,7 @@ func update_camera(is_instant = false):
 		grid.size.x / get_viewport().size.x, 
 		grid.size.y / get_viewport().size.y
 	) * 1.2
-	var new_position = grid.size / 2
+	var new_position = grid.size / 2 + Vector2.DOWN * 15
 	if is_instant:
 		camera.position = new_position
 		camera.zoom = new_zoom
@@ -56,16 +57,32 @@ func update_camera(is_instant = false):
 			camera.zoom, new_zoom, 1,
 			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 		camera_tween.start()
+		
+	var y_pos = new_position.y + 50 * new_zoom.y
+	var font_size = new_zoom.x * 100
+	if Globals.num_units == 1:
+		y_pos = new_position.y - 80
+	elif Globals.num_units == 2:
+		y_pos = new_position.y - 15
+	elif Globals.num_units == 3:
+		y_pos = new_position.y + 30
+	elif Globals.num_units >= 4 and Globals.num_units <= 6:
+		y_pos = new_position.y - 15
+		font_size = new_zoom.x * 50
+	elif Globals.num_units >= 7 and Globals.num_units <= 9:
+		y_pos = new_position.y - 15
+		font_size = new_zoom.x * 50
+	
 	time_left_label.rect_position = Vector2(
 		-1 * money_label.rect_size.x / 2 + 200,
-		new_position.y 
+		y_pos
 	)
-	time_left_label.get("custom_fonts/font").size = new_zoom.x * 35
+	time_left_label.get("custom_fonts/font").size = font_size
 	money_label.rect_position = Vector2(
 		-1 * money_label.rect_size.x / 2 - 200,
-		new_position.y 
+		y_pos
 	)
-	money_label.get("custom_fonts/font").size = new_zoom.x * 35
+	money_label.get("custom_fonts/font").size = font_size
 	
 
 func _input(event):
@@ -76,6 +93,19 @@ func _input(event):
 #		update_camera()
 	if event.is_action_pressed("ui_cancel"):
 		Globals.time_left = 2
+
+func shake_money():
+	money_label.rect_pivot_offset = money_label.rect_size / 2
+	money_tween.interpolate_property(
+		money_label,
+		"rect_scale",
+		Vector2.ONE * 2,
+		Vector2.ONE * 1,
+		0.5,
+		Tween.TRANS_BOUNCE,
+		Tween.EASE_OUT
+	)
+	money_tween.start()
 
 func buy_unit():
 	if Globals.money >= Globals.cost_grid_unit:
@@ -94,6 +124,7 @@ func _process(delta):
 
 func _on_Grid_on_harvest(value):
 	Globals.money += value
+	shake_money()
 
 func _on_Grid_on_feed():
 	Globals.money -= Globals.cost_feed
